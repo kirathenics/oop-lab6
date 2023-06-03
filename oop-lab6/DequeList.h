6395bd64674e1308384dee0606a88a72d52fbd0a
kirathenics <bka32129@gmail.com>
2023-06-03 21:52:37 +0300

    renamed deque in DequeList

--- oop-lab6/DequeList.h
+++ oop-lab6/DequeList.h
@@ -21,31 +21,31 @@ public:
 	friend ostream& operator<< <T>(ostream& out, const DequeList<T>& object);
 	friend istream& operator>> <T>(istream& in, DequeList<T>& object);
 private:
-	deque<T> stk;
+	deque<T> deq;
 };
 
 template<class T>
 DequeList<T>::DequeList()
 {
-	this->stk = deque<T>(0);
+	this->deq = deque<T>(0);
 }
 
 template<class T>
 DequeList<T>::DequeList(int size)
 {
-	this->stk = deque<T>(size);
+	this->deq = deque<T>(size);
 }
 
 template<class T>
 DequeList<T>::DequeList(const DequeList<T>& object)
 {
-	this->stk = object.stk;
+	this->deq = object.deq;
 }
 
 template<class T>
 DequeList<T>::~DequeList()
 {
-	stk.clear();
+	deq.clear();
 }
 
 template<class T>
@@ -53,7 +53,7 @@ DequeList<T>& DequeList<T>::operator=(const DequeList<T>& object)
 {
 	if (this != &object)
 	{
-		this->stk = object.stk;
+		this->deq = object.deq;
 	}
 	return *this;
 }
@@ -62,12 +62,12 @@ template<class T>
 void DequeList<T>::findMaxAndPush()
 {
 	cout << "Задача 3" << endl;
-	T max = *stk.begin();
-	for (auto it = stk.begin(); it != stk.end(); ++it)
+	T max = *deq.begin();
+	for (auto it = deq.begin(); it != deq.end(); ++it)
 	{
 		if (*it > max) max = *it;
 	}
-	stk.push_back(max);
+	deq.push_back(max);
 	return;
 }
 
@@ -78,11 +78,11 @@ void DequeList<T>::findAndErase()
 	cout << "Введите диапазон значений: ";
 	T left, right;
 	cin >> left >> right;
-	for (auto it = stk.begin(); it != stk.end(); )
+	for (auto it = deq.begin(); it != deq.end(); )
 	{
 		if (*it > left && *it < right)
 		{
-			it = stk.erase(it);
+			it = deq.erase(it);
 		}
 		else
 		{
@@ -97,12 +97,12 @@ void DequeList<T>::AddAverage()
 {
 	cout << "Задача 5" << endl;
 	T average = T();
-	for (auto it = stk.begin(); it != stk.end(); ++it)
+	for (auto it = deq.begin(); it != deq.end(); ++it)
 	{
 		average += *it;
 	}
-	average /= stk.size();
-	for (auto it = stk.begin(); it != stk.end(); ++it)
+	average /= deq.size();
+	for (auto it = deq.begin(); it != deq.end(); ++it)
 	{
 		*it += average;
 	}
@@ -113,7 +113,7 @@ template <class T>
 ostream& operator<<(ostream& out, const DequeList<T>& object)
 {
 	out << "Содержимое списка:" << endl;
-	for (auto it = object.stk.begin(); it != object.stk.end(); ++it)
+	for (auto it = object.deq.begin(); it != object.deq.end(); ++it)
 	{
 		cout << *it << "";
 	}
@@ -125,7 +125,7 @@ template <class T>
 istream& operator>>(istream& in, DequeList<T>& object)
 {
 	cout << "Введите элементы списка:" << endl;
-	for (auto it = object.stk.begin(); it != object.stk.end(); ++it)
+	for (auto it = object.deq.begin(); it != object.deq.end(); ++it)
 	{
 		in >> *it;
 	}
